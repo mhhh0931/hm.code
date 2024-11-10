4a01e0c62d27fbdb07377b7c318a6532bcc78438
{HITOMI} <{mhhh0931@gmail.com}>
2024-11-10 14:51:46 +0900

    レスポンシブ　変更修正

--- single.php
+++ single.php
@@ -9,22 +9,24 @@
         <div class="detail__list">
             <?php if(have_posts()): ?>
                 <?php while(have_posts()): the_post(); ?>
-        
-                <div class="detail__img">
-                <?php
-                $pic = get_field('img'); // 'img'はACFで設定したフィールド名
-                if ($pic) { // 値が存在する場合のみ処理
-                    $pic_url = $pic['url'];
-                    echo '<img src="' . esc_url($pic_url) . '" alt="画像">';
-                } else {
-                    echo '<p>画像が設定されていません。</p>'; // 値がない場合のメッセージ
-                }
-                ?>
-            　　　　<a href="">
-                　　<p class="detail__text">サイトを見る</p>
-            　　　　</a>
-        　　　　　</div>
-        
+               <div class="detail__item">
+                    <div class="detail__img">
+                          <?php
+                          $pic = get_field('img'); // 'img'はACFで設定したフィールド名
+                          if ($pic) { // 値が存在する場合のみ処理
+                              $pic_url = $pic['url'];
+                              echo '<img src="' . esc_url($pic_url) . '" alt="画像">';
+                          } else {
+                              echo '<p>画像が設定されていません。</p>'; // 値がない場合のメッセージ
+                          }
+                          ?>
+                     </div>
+                     <div class="detail__link">
+                        <a href="">
+                            サイトを見る
+                        </a>
+                     </div>
+                    </div>
         　　　　　　<dl class="detail__content">
         　　　　　　    <dt class="detail__content--title">サイト名</dt>
         　　　　　　    <dd class="detail__content--text"><?php the_field('name') ;?></dd>
