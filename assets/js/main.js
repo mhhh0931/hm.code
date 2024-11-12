92bae2686e43ce6b27b030540253b9a1e35fb9bd
{HITOMI} <{mhhh0931@gmail.com}>
2024-11-12 20:52:01 +0900

    スタイル調整

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {new Splide(".splide",
     },
   
     breakpoints: {
-      780: {
+      500: {
         perPage: 1,
         fixedWidth: '80%', // モバイル時の幅
         fixedHeight: 240, // モバイル時の高さ
