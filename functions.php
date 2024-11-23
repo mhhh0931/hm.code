40f28ee8bdfba4a1be10a48bcb0d702176073c57
{HITOMI} <{mhhh0931@gmail.com}>
2024-11-23 14:31:46 +0900

    修正

--- functions.php
+++ functions.php
@@ -118,3 +118,8 @@ add_action('wp_enqueue_scripts', 'splide_scripts');
  * アイキャッチ画像を使用可能にする
  */
 add_theme_support('post-thumbnails');
+
+add_action( 'after_setup_theme', 'register_menu' );
+function register_menu() {
+  register_nav_menu( 'primary', __( 'Primary Menu', 'theme-slug' ) );
+}
