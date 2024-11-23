49573ac63cd6acec4e160058d63c8ad93d2171aa
{HITOMI} <{mhhh0931@gmail.com}>
2024-11-23 16:09:03 +0900

    スタイルファイルの整備

--- functions.php
+++ functions.php
@@ -7,71 +7,71 @@ function enqueue_scripts()
 {
     // 共通ファイルを読み込み
     wp_enqueue_style(
-        'global', // globalという名前を設定
-        get_template_directory_uri().'/assets/styles/global.css', // パス
+        'style', // globalという名前を設定
+        get_template_directory_uri().'/assets/styles/style-2.css', // パス
         array('splide-css'), // splide-cssを先に読み込む
         '',
         'all'
     );
 
-    // front-pageページのみ読み込むファイル
-    if (((is_front_page() && is_home()) || (is_front_page() && !is_home())) && !is_paged()) {
-        wp_enqueue_style(
-            'front-page', // front-pageという名前を設定
-            get_template_directory_uri().'/assets/styles/front-page.css', // パス
-            array('global')// global.cssより後に読み込む
-        );
-    }
+//     // front-pageページのみ読み込むファイル
+//     if (((is_front_page() && is_home()) || (is_front_page() && !is_home())) && !is_paged()) {
+//         wp_enqueue_style(
+//             'front-page', // front-pageという名前を設定
+//             get_template_directory_uri().'/assets/styles/front-page.css', // パス
+//             array('global')// global.cssより後に読み込む
+//         );
+//     }
 
-    // 特定の固定ページ（slug: works）のみ読み込むファイル
-    if (is_page('works')) {
-        wp_enqueue_style(
-            'works', // worksという名前を設定
-            get_template_directory_uri().'/assets/styles/works.css', // パス
-            array('global') // global.cssより後に読み込む
-        );
-    }
+//     // 特定の固定ページ（slug: works）のみ読み込むファイル
+//     if (is_page('works')) {
+//         wp_enqueue_style(
+//             'works', // worksという名前を設定
+//             get_template_directory_uri().'/assets/styles/works.css', // パス
+//             array('global') // global.cssより後に読み込む
+//         );
+//     }
 
-    if (is_page('price')) {
-      wp_enqueue_style(
-          'price', // priceという名前を設定
-          get_template_directory_uri().'/assets/styles/price.css', // パス
-          array('global') // global.cssより後に読み込む
-      );
-  }
+//     if (is_page('price')) {
+//       wp_enqueue_style(
+//           'price', // priceという名前を設定
+//           get_template_directory_uri().'/assets/styles/price.css', // パス
+//           array('global') // global.cssより後に読み込む
+//       );
+//   }
 
-  if (is_page('contact')) {
-    wp_enqueue_style(
-        'contact', 
-        get_template_directory_uri().'/assets/styles/contact.css', // パス
-        array('global') // global.cssより後に読み込む
-    );
-}
-if (is_page('contact-confirm')) {
-    wp_enqueue_style(
-        'contact-confirm', 
-        get_template_directory_uri().'/assets/styles/contact-confirm.css', // パス
-        array('global') // global.cssより後に読み込む
-    );
-}
+//   if (is_page('contact')) {
+//     wp_enqueue_style(
+//         'contact', 
+//         get_template_directory_uri().'/assets/styles/contact.css', // パス
+//         array('global') // global.cssより後に読み込む
+//     );
+// }
+// if (is_page('contact-confirm')) {
+//     wp_enqueue_style(
+//         'contact-confirm', 
+//         get_template_directory_uri().'/assets/styles/contact-confirm.css', // パス
+//         array('global') // global.cssより後に読み込む
+//     );
+// }
 
-if (is_page('contact-thanks')) {
-    wp_enqueue_style(
-        'contact-thanks', 
-        get_template_directory_uri().'/assets/styles/contact-thanks.css', // パス
-        array('global') // global.cssより後に読み込む
-    );
-}
+// if (is_page('contact-thanks')) {
+//     wp_enqueue_style(
+//         'contact-thanks', 
+//         get_template_directory_uri().'/assets/styles/contact-thanks.css', // パス
+//         array('global') // global.cssより後に読み込む
+//     );
+// }
 
 
-    // 通常の投稿のシングルページにだけ適用したい処理
-    if (is_single()) {
-        wp_enqueue_style(
-            'single', // singleという名前を設定
-            get_template_directory_uri().'/assets/styles/single.css', // パス
-            array('global') // global.cssより後に読み込む
-        );
-    }
+//     // 通常の投稿のシングルページにだけ適用したい処理
+//     if (is_single()) {
+//         wp_enqueue_style(
+//             'single', // singleという名前を設定
+//             get_template_directory_uri().'/assets/styles/single.css', // パス
+//             array('global') // global.cssより後に読み込む
+//         );
+//     }
 
     // JSの読み込み
     wp_enqueue_script(
